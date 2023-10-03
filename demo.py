457fc7c9fbfcffadc2b6b451de8c900d70d4c283
hiitselle <146594537+hiitselle@users.noreply.github.com>
2023-10-04 00:55:56 +0100

    Update demo.py
    second trial number 4

--- demo.py
+++ demo.py
@@ -32,8 +32,8 @@ with st.expander("Current Leader"):
     
     index = df['Actual Ranking'].idxmin()
 
-    if(df['Worst Case'].iloc[index] == "1") :
-        st.write(df['Name'].iloc[index] + " :green[Winner]")
+    if(df['Worst Case'].iloc[index] == "1""2"):
+        st.write(df['Name'].iloc[index] + df['Worst Case'].iloc[index] == "1")
          st.write(df['Name'].iloc[index] + " :green[Winner]")
         
     else:
