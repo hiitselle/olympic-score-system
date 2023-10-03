bc565b836db14c1548c03def74a41b5fb4948e0e
hiitselle <146594537+hiitselle@users.noreply.github.com>
2023-10-03 14:39:54 +0100

    Update demo.py
    Winner change

--- demo.py
+++ demo.py
@@ -32,7 +32,7 @@ with st.expander("Current Leader"):
     index = df['Actual Ranking'].idxmin()
 
     if(df['Worst Case'].iloc[index] == "1"):
-        st.write(df['Name'].iloc[index] + " is leading & is Unbeatable!")
+        st.write(df['Name'].iloc[index] + " Winner")
     else:
         st.write(df['Name'].iloc[index] + " is leading & is Beatable!")
 
