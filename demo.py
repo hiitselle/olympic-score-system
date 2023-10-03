7d99fdd4523eaf1429c5271f89455d740ca2e7f8
hiitselle <146594537+hiitselle@users.noreply.github.com>
2023-10-03 15:15:21 +0100

    Update demo.py
    beatable changed to red

--- demo.py
+++ demo.py
@@ -34,7 +34,7 @@ with st.expander("Current Leader"):
     if(df['Worst Case'].iloc[index] == "1"):
         st.write(df['Name'].iloc[index] + " :green[Winner]")
     else:
-        st.write(df['Name'].iloc[index] + " is leading & is Beatable!")
+        st.write(df['Name'].iloc[index] + " :red[is leading & is Beatable!]")
 
 
 def generateInfo(index):
