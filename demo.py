71673c4623b93599e421008eae220086de390d9a
hiitselle <146594537+hiitselle@users.noreply.github.com>
2023-10-17 11:54:09 +0100

    Update demo.py
    else change

--- demo.py
+++ demo.py
@@ -60,7 +60,7 @@ def load_data(sheets_url):
 
 if(genderSel=="Male Semis"):
     df = load_data("https://docs.google.com/spreadsheets/d/12i_7HsoRs74S0FtzN04Uu1WZeToU6AoyOidHAL6WcGE/export?format=csv&gid=1473230761")
-elif(genderSel=="Female Semis"):
+else(genderSel=="Female Semis"):
     df = load_data("https://docs.google.com/spreadsheets/d/12i_7HsoRs74S0FtzN04Uu1WZeToU6AoyOidHAL6WcGE/export?format=csv&gid=1802658245")
 with st.expander("Current Leader"):
     
