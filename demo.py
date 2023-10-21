7c7f5e3624912c7a7462075ad5622b4b31ebe476
hiitselle <146594537+hiitselle@users.noreply.github.com>
2023-10-21 22:40:30 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -124,7 +124,7 @@ def generateInfo(index):
                     <style>
     
                     div[data-testid="stExpander"]:nth-of-type(""" + str(index+5) + """) {
-                         background: 	teal;
+                         background: 	lightteal;
                          color: black; # Expander content color
                     }
 
