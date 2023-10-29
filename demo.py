068ae667739529209c714fec1049645679ceb234
hiitselle <146594537+hiitselle@users.noreply.github.com>
2023-10-29 11:12:04 +0000

    Update demo.py

--- demo.py
+++ demo.py
@@ -108,11 +108,12 @@ def generateInfo(index):
     if(df['Is score complete'].iloc[index] == "1"):
         st.write("Worst Case Position: " + df['Worst Case'].iloc[index])
         st.write("Qualified: " + df['Qualified'].iloc[index])
+        st.write("Points to 1st: " + df['Points to 1st'].iloc[index])
      
     else:
      
         st.write("min needed for 8th: " + df['min needed'].iloc[index])
-        st.write("Points to 1st: " + df['Points to 1st'].iloc[index])
+      
         st.write("Points to 2nd: " + df['Points to 2nd'].iloc[index])
         st.write("Points to 3rd: " + df['Points to 3rd'].iloc[index])
         
